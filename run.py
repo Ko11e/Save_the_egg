f2ef1896059894533b7ab23b685f7ea622234229
Emmelie Kolmodin <e.kolmodin@hotmail.com>
2024-03-12 21:21:48 +0000

    Create function to select drop height

--- run.py
+++ run.py
@@ -20,13 +20,22 @@ YES_NO = ['Y', 'y', 'Yes', 'YES', 'yes', 'N', 'n', 'No', 'NO', 'no']
 #protection = SHEET.worksheet('materials')
 #data = protection.get_all_values()
 
-
 def validation_int(user_input):
     try:
-        int(user_input)
-        return user_input
+        float(user_input)
+        return True
     except ValueError:
         print(f"You have enters a string, please select a number.\n")
-        return 0
+        return False
 
-   
+def choose_height():
+    while True:
+        selected_height = input('Choose the height from which you want to drop the egg [meters]:\n')
+        if validation_int(selected_height):
+            print(f"You have chosen to release the egg from {selected_height} metres.")
+            break
+    return selected_height
+
+
+    
+drop_height = choose_height()
