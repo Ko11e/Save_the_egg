ad6447924f4f8b46e1dbdc3e1c67f1490607b8d5
Emmelie Kolmodin <e.kolmodin@hotmail.com>
2024-03-12 21:04:45 +0000

    Create validation to check if the input is a integer or flaot

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials as cd
+import pandas as pd
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -16,8 +17,16 @@ SHEET = GSPREAD_CLIENT.open('Save_the_egg')
 #Global varibel for validation
 YES_NO = ['Y', 'y', 'Yes', 'YES', 'yes', 'N', 'n', 'No', 'NO', 'no']
 
-protection = SHEET.worksheet('materials')
+#protection = SHEET.worksheet('materials')
+#data = protection.get_all_values()
 
-data = protection.get_all_values()
 
-print(data)
+def validation_int(user_input):
+    try:
+        int(user_input)
+        return user_input
+    except ValueError:
+        print(f"You have enters a string, please select a number.\n")
+        return 0
+
+   
