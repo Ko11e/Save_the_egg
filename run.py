121921c768a4cfe42c12b53a482e982539f6dcdf
Emmelie Kolmodin <e.kolmodin@hotmail.com>
2024-03-19 14:18:05 +0000

    Add docsttring to all functions

--- run.py
+++ run.py
@@ -24,6 +24,9 @@ SHEET = GSPREAD_CLIENT.open('Save_the_egg')
 YES_NO = ['Y', 'y', 'Yes', 'YES', 'yes', 'N', 'n', 'No', 'NO', 'no']
 
 class Highscore:
+    """
+    TEXT
+    """
     def __init__(self, level, names, scores):
         self.level = level
         self.names = names
@@ -59,11 +62,26 @@ class Highscore:
             SHEET.worksheet(self.level).update([[self.scores[i-2]]],f'B{i}')
 
 def clear_screen():
+    """
+    This function waits for 2 seconds and then
+    clears the terminal and prints the title of the game
+    ------------------
+        Parameters
+            No argumnets nedded
+    """
     sleep(2)
     system('clear')
     print(Fore.YELLOW+ pyfiglet.figlet_format("Save the Egg", font = "bulbhead", justify="center") +Style.RESET_ALL)
 
 def end_title():
+    """
+    This function Waits for 2 seconds and 
+    then clears the terminal and prints a text 
+    thanking you for playing the game
+    -------------------
+        Parameters
+            No argumnets nedded
+    """
     sleep(2)
     system('clear')
     print(Fore.YELLOW+ pyfiglet.figlet_format("Thank you for playing", font = "mini", justify ="center"))
@@ -74,6 +92,13 @@ def choose_height():
     Get height from the user that the egg is going to be droped from.
     Run a while loop to collect a valid number that is a integer och a flaot.
     The loop will request input until the input is valid.
+    -------------------------------------------
+        Parameters
+            No arguments is needed
+        
+        Retruns 
+            float: Given number by the user is return as a float.
+
     """
     while True:
         selected_height = input('Choose the height from which you want to drop the egg [meters]:\n')
@@ -88,6 +113,16 @@ def validation_number(user_input, lst=None):
     """
     Inside the try, converts the string value to a flaot.
     Raises a ValueError if the string can't be converted into a flaot.
+    ------------------------------------
+        Parameters
+            user_input : str 
+                String to check if it's a number
+            lst :list, optional 
+                A list with numbers (int) to check if ´user_input´ is in the list. 
+                If ´lst´ is None this function is not executed 
+        
+        Returns
+            boolean : True if the sting  a number and False it it's not.
     """
     try:
         float(user_input)
@@ -106,6 +141,16 @@ def validation_int(input, lst):
     Raises ValueError if string can't be converted into int 
     and TypeError if the nummber is higher the number is 
     higher than the length of the list.
+    ------------------------------------
+        Parameters
+            input : str 
+                string to check if it's a int and if the value is in lst
+            lst :list
+                A list with numbers (int) to check if ´input´ is in the list. 
+        
+        Returns
+            out : boolean 
+                True if the sting  a number and False it it's not.
     """
     try:
         int(input)
@@ -123,6 +168,18 @@ def validation_int(input, lst):
     return True
 
 def validation_yes_no(input):
+    """
+    Checks if the 'input' is a sting that is a Y, y, Yes, YES, yes, N, n, No, NO or no.
+    Raises a error with a string saying that the string provided is not a yes or a no.
+    -------------------------------------------
+        Parameters
+            input : str
+
+        Returns
+            out : boolean
+                True if the ´input´ is a Y, y, Yes, YES, yes, N, n, No, NO or no and False if not.           
+    """
+
     if input in YES_NO:
         return True
     else:
@@ -130,6 +187,9 @@ def validation_yes_no(input):
         return False
 
 def yes_no_question(question):
+    """
+    
+    """
     while True:
         try_again = input(question)
         if validation_yes_no(try_again):
@@ -165,6 +225,16 @@ def select_protection():
 def impact_calculation(height, radius_egg):
     """
     Calculates the force that the egg will be impacted by when they hit the ground.
+    ---------------------------------------------
+        Parameters
+            height : int
+                TEXT
+            radius_egg : float
+                TEXT
+        
+        Returns
+            out : float
+                Text
     """
     g = 9.82 # Average gravity in m/s^2
     mass = 0.05 # Mass of the egg in kg
@@ -175,13 +245,27 @@ def impact_calculation(height, radius_egg):
 
 def randomizing_land_of_egg():
     """
-    Generates a 1 or a 2 which will indicate whether the egg lands horizontally or vertically.
+    Generates a 0 or a 1 which will indicate whether the egg lands horizontally or vertically.
+    --------------------------------------
+        Parameters
+            No arguments nedded
+        
+        Returns:
+            out : int
+                Value return is int that is a 0 or 1
+
     """
+
     return randint(0,1)
 
 def broken_egg():
     """
     Prints a broken egg
+    -------------------
+        Parameters
+            No argumnets nedded
+        Returns:
+            No value but prints a ASCII art of a broken egg
     """
     print("Oooo no, the egg broke\n")
     print("                          ⣠⣄⣀")
@@ -196,6 +280,11 @@ def broken_egg():
 def intact_egg():
     """
     Prints a intact egg
+    ------------------
+        Parameters
+            No argumnets nedded
+        Returns:
+            No value but prints a ASCII art of a egg
     """
     print('You managed to save the egg\n')
     print("    ⣠⣾⣿⣿⣿⣿⣷⣄")
@@ -209,6 +298,16 @@ def intact_egg():
     print("      ⠉⠉⠉⠉ ")
 
 def get_highscore_data(difficulty_level):
+    """
+    Text
+    -------------------
+        Parameters
+            difficulty_level : str
+
+        Returns
+            out : class
+                test
+    """
     sheet_highscore = SHEET.worksheet(difficulty_level)
     data = np.array(sheet_highscore.get_all_values()).T[:,1:]
 
@@ -217,59 +316,82 @@ def get_highscore_data(difficulty_level):
     return highscore_data
 
 def reduce_force_limit(egg, landingposition, impact_force):
+    """
+    Tets
+    -----------------
+        Parameters
+            egg : numpy.ndarray
+                TEXT
+            landingposition : int
+                TEXT
+            impact_force : float
+                TEXT
+        
+        Returns
+            out : numpy.ndarray
+                TEXT
+    """
     procent_impact = impact_force/egg['force_limit'][landingposition]
     reduce_force = np.array([20,30])*procent_impact
 
     return egg['force_limit']-reduce_force
 
 def main():
+    """
+    The main function running the game save the egg.
+    """
     print(Fore.YELLOW+ pyfiglet.figlet_format("Save the Egg", font = "bulbhead", justify="center"))
     print('text explaining the game\n' + Style.RESET_ALL)
     egg = np.array([(0.04, 40),(0.06, 60)], dtype=[('height', float),('force_limit', float)])
     highscore_easy = get_highscore_data('easy')
     score = 0
 
-    while True: 
-        height = choose_height()
-        clear_screen()
+    while True:
+        
+        while True: 
+            height = choose_height()
+            clear_screen()
 
-        material, reduction_of_impact = select_protection()
-        clear_screen()
+            material, reduction_of_impact = select_protection()
+            clear_screen()
 
-        landingposition = randomizing_land_of_egg()
-    
-        impact_force = impact_calculation(height, egg['height'][landingposition])
-        total_impact_force = impact_force - reduction_of_impact
+            landingposition = randomizing_land_of_egg()
+        
+            impact_force = impact_calculation(height, egg['height'][landingposition])
+            total_impact_force = impact_force - reduction_of_impact
 
-        if (total_impact_force) < egg['force_limit'][landingposition]:
-            intact_egg()
-            score += int(impact_force *10)
-            position_on_highscore = highscore_easy.made_highscore(score)
+            if (total_impact_force) < egg['force_limit'][landingposition]:
+                intact_egg()
+                score += int(impact_force *10)
+                position_on_highscore = highscore_easy.made_highscore(score)
 
-            if position_on_highscore != 10:
-                print(f'Woho!! You scored {score} and got on the {position_on_highscore+1}:th place\n')
-                print(highscore_easy)
-                try_again = yes_no_question('\nDo you want to try to increase your score? [Y/N]:')
-
-                if YES_NO.index(try_again) >= 5:
-                    name = input('Enter your name to the highscore list:\n')
-                    highscore_easy.add_to_board(position_on_highscore, name, score)
+                if position_on_highscore != 10:
+                    print(f'Woho!! You scored {score} and got on the {position_on_highscore+1}:th place\n')
                     print(highscore_easy)
-                    highscore_easy.uppdate_sheet()
-                    
+                    try_again = yes_no_question('\nDo you want to try to increase your score? [Y/N]:')
+
+                    if YES_NO.index(try_again) >= 5:
+                        name = input('Enter your name to the highscore list:\n')
+                        highscore_easy.add_to_board(position_on_highscore, name, score)
+                        print(highscore_easy)
+                        highscore_easy.uppdate_sheet()
+                        break
+                        
+                    else:
+                        egg['force_limit'] = reduce_force_limit(egg, landingposition, total_impact_force)
+
+
                 else:
-                    egg['force_limit'] = reduce_force_limit(egg, landingposition, total_impact_force)
+                    print(f'\nYou scored {score} points and your score did not make the top 5')
+                    try_again = yes_no_question('\nDo you want to try to increase your score? [Y/N]:')
 
-
+                    if YES_NO.index(try_again) < 5:
+                        egg['force_limit'] = reduce_force_limit(egg, landingposition, impact_force)
+                    else:
+                        break    
             else:
-                print(f'\nYou scored {score} points and your score did not make the top 5')
-                try_again = yes_no_question('\nDo you want to try to increase your score? [Y/N]:')
-
-                if YES_NO.index(try_again) < 5:
-                    egg['force_limit'] = reduce_force_limit(egg, landingposition, impact_force)
-                    
-        else:
-            broken_egg()
+                broken_egg()
+                break
         
         play_again = yes_no_question('Do you want to play again? [Y/N]')
         
@@ -284,11 +406,3 @@ def main():
 
 
 main()
-# print(pyfiglet.figlet_format("Save the Egg", font = "bulbhead" ))
-
-# highscore(50)
-#sheet_highscore = SHEET.worksheet('easy')
-#data_easy = np.array(sheet_highscore.get_all_values()).T[:,1:]
-
-#print(data_easy[0], data_easy[1])
-
