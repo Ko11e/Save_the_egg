77fb3002c41ccc079c14a8b5fc386c4754375334
Emmelie Kolmodin <e.kolmodin@hotmail.com>
2024-03-22 10:35:35 +0000

    Change terminal size

--- controllers/default.js
+++ controllers/default.js
@@ -18,8 +18,8 @@ function socket() {
         // Spawn terminal
         client.tty = Pty.spawn('python3', ['run.py'], {
             name: 'xterm-color',
-            cols: 150,
-            rows: 50,
+            cols: 100,
+            rows: 35,
             cwd: process.env.PWD,
             env: process.env
         });
